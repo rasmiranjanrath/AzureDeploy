eee9617faff75da3117e0fe5164b76f77ac4fb20
Rasmiranjan Rath <rasmiranjanrath@Rasmiranjans-MacBook-Air.local>
2025-07-28 13:46:38 +0530

    initial commit

---
+++ src/test/java/com/example/TestDeploy/TestDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestDeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
